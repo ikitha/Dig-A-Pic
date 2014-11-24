8170de429d07967cc31009eeabec0d2a83a7f617
JamesDuffy <jaduffy007@gmail.com>
2014-11-24 10:15:58 -0800

    work on photos index

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "jaduffy108+info@gmail.com"
+  default from: "digapic@yahoo.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
