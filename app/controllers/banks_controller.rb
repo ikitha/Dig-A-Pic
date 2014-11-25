3590714e24a65339c336314ccf16e7093db884a2
Myla Baker <myla.baker@gmail.com>
2014-11-24 16:51:44 -0800

    fixed adding bank

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,25 +1,29 @@
 class BanksController < ApplicationController
+  before_action :current_user
+
   def new
+    @bank = Bank.new
   	@user = User.find_by_id(session[:user_id])
   end
 
   def create
-  	if bank_params[:user_id] != session[:user_id]
+    binding.pry
+  	if params[:user_id].to_i != @current_user.id
   		redirect_to all_photos_path
   	else
 
-  	user = User.find_by_id(session[:user_id])
+  	user = User.find_by_id(@current_user.id)
   	bank_account = Balanced::BankAccount.new(
-		  :account_number => bank_params[:account_number],
+		  :account_number => params[:account_number],
 		  :account_type => 'checking',
 		  :name => "#{user.firstname} #{user.lastname}",
-		  :routing_number => bank_params[:routing_number]
+		  :routing_number => params[:routing_number]
 		).save
 
   	bank_account.associate_to_customer(user.balanced_href)
 
   	bank = Bank.create(:user_id => user.id, :balanced_href => bank_account.attributes[:href])
-
+    binding.pry
 
 
 
@@ -33,6 +37,6 @@ class BanksController < ApplicationController
 
   private
   def bank_params
-    params.permit(:account_number, :routing_number, :user_id)
+    params.require(:bank).permit(:balanced_href, :user_id)
   end
 end
